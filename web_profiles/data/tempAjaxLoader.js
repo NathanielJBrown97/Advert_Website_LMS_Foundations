cdf30d47179fcd0e64bd22bd37e16df063fe80fe
Nathaniel <nathaniel.j.brown.97@gmail.com>
2024-03-01 14:04:51 -0500

    Adjusted text displays; added clear so the page doens't get so clunky.

--- web_profiles/data/tempAjaxLoader.js
+++ web_profiles/data/tempAjaxLoader.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
     // Load HTML content
     $("#loadHtmlBtn").click(function() {
+        clearContent(); // clear any existing content
         $("#htmlContent").load("../../data/tempContent.html");
     });
 
     // Load and process JSON data
     $("#loadJsonBtn").click(function() {
+        clearContent(); // clear any existing content
         $.getJSON("../../data/tempContent.json", function(data) {
             var items = [];
             items.push("<h3>" + data.title + "</h3><p>" + data.description + "</p>");
@@ -18,6 +20,7 @@ $(document).ready(function() {
 
     // Load HTML content with jQuery
     $("#loadJqueryContentBtn").click(function() {
+        clearContent(); // clear any existing content
         $.ajax({
             url: "../../data/tempJqueryContent.html",
             success: function(result) {
